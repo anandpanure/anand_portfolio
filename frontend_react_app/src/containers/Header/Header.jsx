22ae43689fefb5c4652769d6979ae46a149809b8
Anand Panure <anand.panure@gmail.com>
2022-06-25 00:56:25 -0700

    Adding Wrapper to components

--- frontend_react_app/src/containers/Header/Header.jsx
+++ frontend_react_app/src/containers/Header/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import './Header.scss'
 import { motion } from 'framer-motion';
+import {AppWrap} from '../../wrapper';
 import { images } from '../../constants'
+import './Header.scss'
 
 const scaleVariants ={
   whileInView:{
@@ -16,7 +17,7 @@ const scaleVariants ={
 
 const Header = () => {
   return (
-    <div id="home" className='app__header app__flex'>
+    <div className='app__header app__flex'>
       <motion.div whileInView={{x:[-100,0], opacity: [0,1]}}
       transition={{duration:0.5}}
       className="app__header-info"
@@ -56,11 +57,11 @@ const Header = () => {
       whileInView={scaleVariants.whileInView}
       className="app__header-circles"
       >
-        {[images.flutter, images.redux, images.sass].map((circle, index) => (
-                    <div className='circle-cmp app__flex' key={'circle-${index}'}>
-                    <img src={circle} alt="circle"/>
-                  </div>
-        ))}
+      {[images.flutter, images.redux, images.sass].map((circle, index) => (
+        <div className="circle-cmp app__flex" key={`circle-${index}`}>
+          <img src={circle} alt="profile_bg" />
+        </div>
+      ))}
 
       </motion.div>
 
@@ -68,4 +69,4 @@ const Header = () => {
   )
 }
 
-export default Header
+export default AppWrap(Header, 'home');
