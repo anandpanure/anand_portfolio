46ec8419207928c0afc750ecc99f66c7dc7fbc20
Anand Panure <anand.panure@gmail.com>
2022-06-25 23:53:34 -0700

    Adding work section from sanity

--- frontend_react_app/src/containers/Work/Work.jsx
+++ frontend_react_app/src/containers/Work/Work.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React , {useState,useEffect} from 'react'
+import { AiFillEye, AiFillGithub} from 'react-icons/ai'
+import {motion} from 'framer-motion'
+import {AppWrap} from '../../wrapper'
+import {urlFor, client} from '../../client'
+
+
 import './Work.scss'
 
 const Work = () => {
+  const [activeFilter, setactiveFilter] = useState('All');
+  const [animateCard, setanimateCard] = useState({y:0, opacity:1});
+
+  const [works, setWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState([])
+
+  useEffect(() => {
+    const query = '*[_type == "work"]';
+
+    client.fetch(query)
+    .then((data) => {
+      setWorks(data);
+      setFilterWork(data);
+    })
+  }, [])
+  
+
+  const handleWorkFilter = (item) => {
+
+  }
   return (
-    <div>Work</div>
+    <>
+      <h2 className='head-text'>My Creative <span>Portfolio</span> Section</h2>
+
+      <div className='app__filter'>
+        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item,index) => (
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active':''}`}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+
+      <motion.div
+        animate={animateCard}
+        transition={{duration:0.5, delayChildren:0.5}}
+        className="app__work-portfolio"
+      >
+        {filterWork.map((work,index) => (
+          <div className="app__work-item app__flex" key={index}>
+            <div className="app__work-img app__flex">
+
+            </div>
+          </div>
+        ))}
+      </motion.div>
+    </>
   )
 }
 
